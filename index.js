d87af6eb6cd1c1d25b6a480328984fcbbc851c01
Iris Quintero <godessiris@gmail.com>
2019-04-08 05:45:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,29 +5,5 @@ function happyHolidays(){
 
 function happyHolidaysTo(name){
   return `Happy holidays, ${name}!`;
-  var name = "you"
-}
-
- function happyHolidayTo(holiday, name) {
-  var holiday = "Independence Day";
   var name = "you";
-  return `Happy ${holiday}, ${name}!`
-}
-
- function holidayCountdown(holiday, days) {
-  return `It's ${days} days until ${holiday}!`
-}
-  
-  
-
- 
-}
-
-
-function happyHolidaysTo(holiday, name){
-  return(`Happy ${holiday}, ${name}!`)
-}
-
-function holidayCountdown(days, holiday){
-  return(`It's ${days} days until ${holiday}!`)
 }
