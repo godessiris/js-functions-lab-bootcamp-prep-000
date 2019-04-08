5b3a59227546d86f2de3f765e80bb9a347a7f96d
Iris Quintero <godessiris@gmail.com>
2019-04-08 05:54:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,6 +14,15 @@ function happyHolidayTo(holiday, name){
   var name = "you";
 }
 
+function holidayCountdown(holiday, days){
+  return `It\'s ${days} days until ${holiday}!`;
+  var holiday = "Mother's Day";
+  var days = "20";
+}
 
 
+
+  describe('holidayCountdown(holiday, days)', () => {
+    it('returns "It\'s ${days} days until ${holiday}!"', () => {
+      expect(holidayCountdown("Mother's Day", 20)).toEqual("It's 20 days until Mother's Day!")
 
