5fc0ce7884e19ea8f1232c3f5345d08d80c5433f
Iris Quintero <godessiris@gmail.com>
2019-04-08 05:41:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,7 @@
 function happyHolidays(){
   return(`Happy holidays!`)
 }
-
-  function happyHolidaysTo(name){
+function happyHolidaysTo(name){
   return `Happy holidays, ${name}!`;
   var name = "you"
 }
